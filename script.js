87caaa0c98370c991efccf14195e70d67ecab23b
Emi <emibada123@gmail.com>
2022-12-08 20:55:07 -0300

    uptade idiomas

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ window.addEventListener('scroll', function(){
     }
 })
 
-
+//español
 let ciruclarProgress = document.querySelector(".circular-progress"), progressValue = document.querySelector(".progress-value");
-let progressStartValue = 0, progressEndValue = 100, speed = 15;
+let progressStartValue = 0, progressEndValue = 100, speed = 50;
 
 let progress = setInterval(() => {
     progressStartValue++
@@ -22,6 +22,44 @@ let progress = setInterval(() => {
         clearInterval(progress);
     }
 }, speed);
+
+
+
+//ingles
+let circularProgress2 = document.querySelector(".circular-progressDOS"),
+    progressValue2 = document.querySelector(".progress-valueDOS");
+
+let progressStartValue_ = 0,
+    progressEndValue_ = 50,
+    speed_ = 50;
+
+let progress_ = setInterval(() => {
+    progressStartValue_++
+    progressValue2.textContent = progressStartValue_ + "%"
+    circularProgress2.style.background = "conic-gradient(rgba(1,56,105,1)" + progressStartValue_ * 3.6 + "deg, white 0deg)";
+    if (progressStartValue_ == progressEndValue_){
+        clearInterval(progress_)
+    }
+}, speed_);
+
+
+//frances
+let circularProgress3 = document.querySelector(".circular-progressTRES"),
+    progressValue3 = document.querySelector(".progress-valueTRES");
+
+let progressStartValue_3 = 0,
+    progressEndValue_3 = 30,
+    speed_3 = 50;
+
+let progress_3 = setInterval(() => {
+    progressStartValue_3++
+    progressValue3.textContent = progressStartValue_3 + "%"
+    circularProgress3.style.background = "conic-gradient(rgba(1,56,105,1)" + progressStartValue_3 * 3.6 + "deg, white 0deg)";
+    if (progressStartValue_3 == progressEndValue_3){
+        clearInterval(progress_3)
+    }
+}, speed_);
+
 
 
 
